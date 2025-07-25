7ea3b20559b4a5bdaae36345c4de17a695c5e373
ManmeetSingh1012 <manmeetsinghnss@gmail.com>
2025-07-25 20:03:05 +0530

    cors error

--- server.js
+++ server.js
@@ -26,7 +26,9 @@ app.use(cors({
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://localhost:3001",
-    "http://127.0.0.1:3001"
+    "http://127.0.0.1:3001",
+    "https://www.checkinsuite.in",
+    "https://www.checkinsuite.in/"
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
